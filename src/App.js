73ff779116623148bf3bfae00f3fc4422b14f645
Jose Herrera <herrera@Joses-MacBook-Pro.local>
2022-02-11 18:04:45 -0500

    refactor React project, its running good making several tests

--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@ import React from 'react';
 import Calculator from './components/Calculator';
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
+const App = () => {
     return (
       <div className="container">
         <Calculator />
       </div>
     );
   }
-}
+
 
 export default App;
