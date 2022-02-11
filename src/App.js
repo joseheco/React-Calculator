6ca93f98aacfee848cd16084b7da415cea5bf297
Jose Herrera <herrera@Joses-MacBook-Pro.local>
2022-02-11 18:14:12 -0500

    fix linter

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React from 'react';
 import Calculator from './components/Calculator';
 import './App.css';
 
-const App = () => {
-    return (
-      <div className="container">
-        <Calculator />
-      </div>
-    );
-  }
-
+const App = () => (
+  <div className="container">
+    <Calculator />
+  </div>
+);
 
 export default App;
